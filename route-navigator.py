06f60d4245d077e2d514d3bab205b830df3ddae3
Basil Mari <basilm4r0@gmail.com>
2022-05-17 02:12:08 +0300

    fix errors and mistake in spreadsheet

--- route-navigator.py
+++ route-navigator.py
@@ -43,7 +43,10 @@ class City_Node(Node):
                  goal = 0):
 
         super(City_Node, self).__init__(name, parent, start, goal)
-        self.cost = parent.cost + cost
+        if parent:
+            self.cost = float(parent.cost) + float(cost)
+        else:
+            self.cost = cost
         self.aerialdist = self.GetAerialDistance()
         self.walkindist = self.GetWalkingDistance()
 
@@ -51,29 +54,29 @@ class City_Node(Node):
 
         if self.name == self.goal:
             return 0
-        o = SearchList(aerial_list, self.name)
-        i = SearchList(aerial_list, self.goal)
-        aerialdist = sheet.cell_value(o,i)
+        o = SearchSheet(aerial_sheet, self.name)
+        i = SearchSheet(aerial_sheet, self.goal)
+        aerialdist = float(aerial_sheet.cell_value(o,i))
         return aerialdist
 
     def GetWalkingDistance(self):
 
         if self.name == self.goal:
             return 0
-        o = SearchList(walking_list, self.name)
-        i = SearchList(walking_list, self.goal)
-        walkingdist = sheet.cell_value(o,i)
+        o = SearchSheet(walking_sheet, self.name)
+        i = SearchSheet(walking_sheet, self.goal)
+        walkingdist = float(walking_sheet.cell_value(o,i))
         return walkingdist
 
     def CreateChildren(self):
         if not self.children:
             o = SearchSheet(driving_sheet, self.name)
-            for i in range(driving_sheet.ncols):
+            for i in range(1, driving_sheet.ncols):
                 
                 if (str(driving_sheet.cell_value(o,i)) != ""):
                     name = driving_sheet.cell_value(0,i)
-                child = City_Node(name, self, driving_sheet.cell_value(o,i)
-                self.children.append(child)
+                    child = City_Node(name, self, float(driving_sheet.cell_value(o,i)))
+                    self.children.append(child)
 
 class AStar_Solver:
     def __init__(self, start , goal, heuristic):
@@ -83,24 +86,27 @@ class AStar_Solver:
         self.start         = start
         self.goal          = goal
         self.heuristric    = heuristic
+        self.cost          = 0
 
     def Solve(self):
         startState = City_Node(self.start, 0, 0,
                                   self.start,
                                   self.goal)
 
-        self.priorityQueue.put((startState.cost + startState.aerialdist, startState))
-
+        count = 0
+        self.priorityQueue.put((startState.cost + startState.aerialdist, count, startState))
         while(not self.path and self.priorityQueue.qsize()):
-            closestChild = self.priorityQueue.get()[1]
+            closestChild = self.priorityQueue.get()[2]
             closestChild.CreateChildren()
             self.visitedQueue.append(closestChild.name)
             if (closestChild.name == self.goal):
                 self.path = closestChild.path
+                self.cost = closestChild.cost
                 break
             for child in closestChild.children:
                 if child.name not in self.visitedQueue:
-                    self.priorityQueue.put((child.cost + child.aerialdist, child))
+                    count += 1
+                    self.priorityQueue.put((child.cost + child.aerialdist, count, child))
 
         if not self.path:
             print("Goal of %s is not possible!" % (self.goal))
@@ -128,3 +134,4 @@ if __name__ == "__main__":
 
     for i in range(len(a.path)):
         print("{0}) {1}".format(i, a.path[i]))
+    print("Path cost: %s" % a.cost)
